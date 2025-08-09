8824c6ef54d05532c7ab4d32f73214ddc2a0cfa5
Mohammed Ashiq <mohammedashiq836@gmail.com>
2025-08-09 18:02:49 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Task Manager",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Scaffold(),
+    );
+  }
+}
