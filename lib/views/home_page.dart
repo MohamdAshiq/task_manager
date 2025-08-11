47c9da5dacc6971c528c400bb94b58f548d77557
Mohammed Ashiq <mohammedashiq836@gmail.com>
2025-08-11 10:43:33 +0530

    Implemented Pending Tasks Listing Section.

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -8,6 +8,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        scrolledUnderElevation: 0,
         title: Text(
           "TASK MANAGER",
           style: TextStyle(
@@ -17,7 +19,74 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(children: [TaskStatsCardsSection()]),
+      body: SafeArea(
+        child: Column(
+          children: [
+            ColoredBox(
+              color: Theme.of(context).scaffoldBackgroundColor,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TaskStatsCardsSection(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 5,
+                    ),
+                    child: Text(
+                      "Pending Tasks",
+                      style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.separated(
+                separatorBuilder: (context, index) => SizedBox(height: 10),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                itemCount: 20,
+                itemBuilder: (context, index) => ListTile(
+                  minTileHeight: 70,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(10),
+                  ),
+                  contentPadding: EdgeInsets.only(top: 5, bottom: 5, left: 15),
+                  tileColor: const Color.fromARGB(255, 27, 27, 27),
+                  leading: Text(
+                    "${index + 1}",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  title: Text(
+                    "Task Title ${index + 1}",
+                    style: TextStyle(fontFamily: "Poppins"),
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    spacing: 5,
+                    children: [
+                      IconButton(
+                        tooltip: "Mark as Completed",
+                        onPressed: () {},
+                        icon: Icon(Icons.done, color: Colors.greenAccent),
+                      ),
+                      IconButton(
+                        tooltip: "Delete Task",
+                        onPressed: () {},
+                        icon: Icon(Icons.close, color: Colors.redAccent),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
