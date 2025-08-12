cf2f4cc33d7d317c927783947e40d7bf9e217204
Mohammed Ashiq <mohammedashiq836@gmail.com>
2025-08-12 16:53:05 +0530

    Created Task Details Screen.

--- lib/widgets/task_stats_card.dart
+++ lib/widgets/task_stats_card.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:task_manager/notifiers/tasks_controller.dart';
+import 'package:task_manager/views/task_details_screen.dart';
 
 class TaskStatsCard extends StatelessWidget {
   const TaskStatsCard({
@@ -14,37 +17,58 @@ class TaskStatsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 110,
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 40, 40, 40),
-        borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => TaskDetailsScreen(
+            title: title,
+            tasks: title == "All Tasks"
+                ? context.read<TasksController>().allTasks
+                : context
+                      .read<TasksController>()
+                      .allTasks
+                      .where(
+                        (element) => title == "Completed"
+                            ? element.isCompleted
+                            : !element.isCompleted,
+                      )
+                      .toList(),
+          ),
+        ),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          SizedBox(),
-          Text(
-            taskCount.toString(),
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-              fontFamily: "Poppins",
+      child: Container(
+        height: 100,
+        width: 110,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 40, 40, 40),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(),
+            Text(
+              taskCount.toString(),
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                fontFamily: "Poppins",
+              ),
             ),
-          ),
-          Text(
-            title.toUpperCase(),
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.bold,
-              fontFamily: "Poppins",
-              color: titleColor,
-              letterSpacing: 1,
+            Text(
+              title.toUpperCase(),
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                fontFamily: "Poppins",
+                color: titleColor,
+                letterSpacing: 1,
+              ),
             ),
-          ),
-          SizedBox(),
-        ],
+            SizedBox(),
+          ],
+        ),
       ),
     );
   }
